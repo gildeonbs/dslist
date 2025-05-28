9f08f334a463b92b63fe2b8612e143e25e404fa5
Gildeon Santos <gildeon23@gmail.com>
2025-05-27 23:45:51 -0300

    GET game by id

--- src/main/java/com/gildeonbs/dslist/controllers/GameController.java
+++ src/main/java/com/gildeonbs/dslist/controllers/GameController.java
@@ -2,11 +2,13 @@ package com.gildeonbs.dslist.controllers;
 
 import java.util.List;
 
+import com.gildeonbs.dslist.dto.GameDTO;
 import com.gildeonbs.dslist.dto.GameMinDTO;
 import com.gildeonbs.dslist.entities.Game;
 import com.gildeonbs.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,11 @@ public class GameController {
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findAll();
         return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 
 
--- src/main/java/com/gildeonbs/dslist/services/GameService.java
+++ src/main/java/com/gildeonbs/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.gildeonbs.dslist.services;
 
+import com.gildeonbs.dslist.dto.GameDTO;
 import com.gildeonbs.dslist.dto.GameMinDTO;
 import com.gildeonbs.dslist.entities.Game;
 import com.gildeonbs.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +18,18 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    // Spring's @Transactional annotation manages the transaction scope for the method
+    // The "readOnly = true" flag optimizes performance for read operations by avoiding unnecessary locking and persistence context flushing
+    // This is useful when the method only reads data and doesn't perform any updates or inserts
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        // TODO: Improve this by handling the case where the given ID does not exist
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
     // Returns a list of GameMinDTO objects
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         // Stream is a Java library that allows operations on sequences of data.
@@ -26,4 +39,6 @@ public class GameService {
         List<GameMinDTO> gameMinDTOS = result.stream().map(x -> new GameMinDTO(x)).toList();
         return gameMinDTOS;
     }
+
+
 }
